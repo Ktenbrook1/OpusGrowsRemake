747f96f89c579a890a786c8d6b98b02bbe32cbde
Katelynn <katelynntenbrook@gmail.com>
2020-11-04 10:10:45 -0600

    My Opus grows remake I made in June 2020

---
+++ script.js
@@ -0,0 +1,9 @@
+function onNav(){
+    var x = document.getElementById("myLinks");
+    if(x.style.display === "inline-block"){
+        x.style.display = "none";
+    }
+else{
+    x.style.display = "inline-block";
+    }
+}
